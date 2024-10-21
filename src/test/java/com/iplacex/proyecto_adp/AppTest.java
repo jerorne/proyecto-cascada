686f99481482ed59fc5cc41b1954e12f04c8d242
JJ <53882119+jerorne@users.noreply.github.com>
2024-10-21 01:38:06 -0300

    Update AppTest.java

--- src/test/java/com/iplacex/proyecto_adp/AppTest.java
+++ src/test/java/com/iplacex/proyecto_adp/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Iplacex!");
     }
 }
